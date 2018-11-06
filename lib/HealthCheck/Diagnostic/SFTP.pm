0dc6291a0945da15c006199f62064570278d90d1
Brandon Messineo <brandon.messineo@grantstreet.com>
2018-11-06 15:13:58 -0500

    Create the constructor method
    We want to allow the user to pass in a hashref or hash of parameters. We
    also want to automatically append the diagnostic label and id to the
    constructed object.

--- lib/HealthCheck/Diagnostic/SFTP.pm
+++ lib/HealthCheck/Diagnostic/SFTP.pm
@@ -7,6 +7,19 @@ use parent 'HealthCheck::Diagnostic';
 use strict;
 use warnings;
 
+sub new {
+    my ($class, @params) = @_;
+
+    my %params = @params == 1 && ( ref $params[0] || '' ) eq 'HASH'
+        ? %{ $params[0] } : @params;
+
+    return $class->SUPER::new(
+        id    => 'sftp',
+        label => 'sftp',
+        %params,
+    );
+}
+
 1;
 __END__
 
